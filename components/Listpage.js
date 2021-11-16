cfe0e699740735f76611c9578eab21467fc58609
Flora <mazzavillanif@gmail.com>
2021-11-16 12:37:29 +1100

    insalled firebase and navigation

---
+++ components/Listpage.js
@@ -0,0 +1,10 @@
+import React from 'react'; 
+import {View, Text, StyleSheet} from "react-native";
+
+export function Listpage ( props ) {
+    return (
+    <View>
+        <Text> List Page </Text>
+    </View>
+    )
+}
---
+++ components/Singleexercise.js
@@ -0,0 +1,10 @@
+import React from 'react'; 
+import {View, Text, StyleSheet} from "react-native";
+
+export function Singleexercise ( props ) {
+    return (
+    <View>
+        <Text> Singleexercise page </Text>
+    </View>
+    )
+}
