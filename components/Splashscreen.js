b6f21131718ae233bc35a548511429e4dc93c34d
Flora <mazzavillanif@gmail.com>
2021-11-24 21:59:30 +1100

    splash screen still not working

--- components/Splashscreen.js
+++ components/Splashscreen.js
@@ -1,25 +1,25 @@
 import React, {useEffect} from 'react'; 
-import {StyleSheet, Image, Text, View, ComponentContainer} from "react-native";
-import { ThemeColors } from './ThemeColours';
+import {StyleSheet, Image, ComponentContainer} from "react-native";
+import { ThemeColours } from './ThemeColours';
 
 export function Splashscreen ({navigation}) {
 
-useEffect (() => {setTimeout(() => {navigation.navigate('Signup');},25000)})
+useEffect (() => {setTimeout(() => {navigation.navigate('Signup');},2500)})
     
-    return (
-        <ComponentContainer style={styles.background}>
-            <Image
-                source={require('./assets/yellowlogo.png')}
-            />
-        </ComponentContainer>
+return (
+    <ComponentContainer style={styles.background}>
+        <Image
+            source={require('../assets/yellowlogo.png')}
+        />
+    </ComponentContainer>
     ); 
 }
 
 const styles = StyleSheet.create({
     background: {
-      flex: 1,
-      backgroundColor: ThemeColors.rawSienna,
-      alignItems: 'center',
-      justifyContent: 'center',
+    flex: 1,
+    backgroundColor: ThemeColours.rawSienna,
+    alignItems: 'center',
+    justifyContent: 'center',
     },
-  });
+}); 
