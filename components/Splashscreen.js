4c84924ff28b2f91afa1024a2e22e6fc4b7bbc11
Flora <mazzavillanif@gmail.com>
2021-11-27 11:56:55 +1100

    splash screen designed & working

--- components/Splashscreen.js
+++ components/Splashscreen.js
@@ -1,25 +1,32 @@
 import React, {useEffect} from 'react'; 
-import {StyleSheet, Image, ComponentContainer} from "react-native";
+import {StyleSheet, Image, ComponentContainer, View} from "react-native";
 import { ThemeColours } from './ThemeColours';
 
 export function Splashscreen ({navigation}) {
 
-useEffect (() => {setTimeout(() => {navigation.navigate('Signup');},2500)})
+useEffect (() => {setTimeout(() => {navigation.navigate('Signup')},2500)})
     
 return (
-    <ComponentContainer style={styles.background}>
-        <Image
+    <View style={styles.background}>
+        <Image style={styles.image}
             source={require('../assets/yellowlogo.png')}
         />
-    </ComponentContainer>
+    </View>
     ); 
 }
 
 const styles = StyleSheet.create({
     background: {
     flex: 1,
-    backgroundColor: ThemeColours.rawSienna,
+    backgroundColor: ThemeColours.maximumYellowRed,
     alignItems: 'center',
     justifyContent: 'center',
+    },
+    image: {
+    flex: 1,
+    width: 450,
+    height: 450,
+    resizeMode: 'contain',
+    alignItems: 'center',
     },
 }); 
